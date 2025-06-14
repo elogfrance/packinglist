7c5947746058581c2cb768964d7fad9a5beb6e51
elogfrance <Mehdi.b@e-log-france.com>
2025-06-14 11:52:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,6 @@ col1, col2 = st.columns(2)
 with col1:
     uploaded_f1 = st.file_uploader("📁 1. Importer le fichier TO SHIP", type=["xlsx"], key="f1")
     
-    with st.expander("📎 Voir le template (F1)"):
-        st.image("template_fichier_to_ship.png", caption="template")
 
 
 with col2:
