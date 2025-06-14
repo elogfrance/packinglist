a65239076e8b629959252918e08598f5763e63ef
elogfrance <Mehdi.b@e-log-france.com>
2025-06-14 10:19:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 
 # Affichage du logo
 logo = Image.open("marketparts.jpg")
-st.image(logo, width=200)
+st.image(logo, width=300)
 
 # Titre principal
 st.markdown("<h1 style='color:#3a4e9f;'>📦 Générateur de Packing List</h1>", unsafe_allow_html=True)
