ebe966f052850f1ddc966dae507e578850da21be
elogfrance <Mehdi.b@e-log-france.com>
2025-06-15 21:17:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,29 @@ params = st.experimental_get_query_params()
 tool = params.get("tool", [None])[0]
 
 
-st.set_page_config(page_title="Outils E-LOG", layout="centered")
+# ➌ Routing selon le paramètre 'tool'
+if tool == "packing_list":
+    import applications.packing_list as pl
+    pl.run()
+
+elif tool == "autodoc":
+    import applications.packing_list_autodoc as autodoc
+    autodoc.run()
+
+else:
+    # Menu principal
+    st.markdown(
+        "<h1 style='text-align: center; color: #3a4e9f;'>🧰 Outils E-LOG</h1>",
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        "<p style='text-align: center;'>Cliquez sur une vignette pour ouvrir l’outil dans un nouvel onglet :</p>",
+        unsafe_allow_html=True
+    )
+
+    col1, col2 = st.columns(2)
+    # On ajoutera les liens à l’étape suivante
+
 
 st.markdown(
     "<h1 style='text-align: center; color: #3a4e9f;'>🧰 Outils E-LOG</h1>",
