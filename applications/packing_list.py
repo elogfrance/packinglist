b7ac360b46f6011a741f471885ee31cba9126d71
elogfrance <Mehdi.b@e-log-france.com>
2025-06-16 00:12:33 +0200

    Update packing_list.py

--- applications/packing_list.py
+++ applications/packing_list.py
@@ -1,9 +1,9 @@
-
 import streamlit as st
 
 def run():
     st.header("📦 Générateur de packing list")
     st.info("⚙️ Le traitement complet sera intégré ici.")
+
 
 import pandas as pd
 from openpyxl import load_workbook
